484a92ba244aa2f6c063515e539bb44d8bca066e
Doug Kintop <dkintop@hotmail.com>
2019-10-21 21:44:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,9 @@ class App < Sinatra::Base
   end
 
   get '/friends' do
-    
+     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+ 
+  erb :friends
 
   end
 end
